ad122ded5696a80f199431a1e4a4362909d28ad8
wuyuetiger <wuyuetiger@gmail.com>
2014-04-19 21:06:11 +0800

    4/19

---
+++ src/org/sosostudio/dbunifier/oom/InsertKeyValueClause.java
@@ -0,0 +1,108 @@
+package org.sosostudio.dbunifier.oom;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import org.sosostudio.dbunifier.Values;
+
+public class InsertKeyValueClause {
+
+	private StringBuilder keysSb = new StringBuilder();
+
+	private StringBuilder valuesSb = new StringBuilder();
+
+	private Values values = new Values();
+
+	public InsertKeyValueClause addStringClause(String columnName,
+			String stringValue) {
+		if (keysSb.length() > 0) {
+			keysSb.append(", ");
+			valuesSb.append(", ");
+		}
+		keysSb.append(columnName);
+		if (stringValue == null) {
+			valuesSb.append("null");
+		} else {
+			valuesSb.append("?");
+			values.addStringValue(stringValue);
+		}
+		return this;
+	}
+
+	public InsertKeyValueClause addNumberClause(String columnName,
+			BigDecimal numberValue) {
+		if (keysSb.length() > 0) {
+			keysSb.append(", ");
+			valuesSb.append(", ");
+		}
+		keysSb.append(columnName);
+		if (numberValue == null) {
+			valuesSb.append("null");
+		} else {
+			valuesSb.append("?");
+			values.addNumberValue(numberValue);
+		}
+		return this;
+	}
+
+	public InsertKeyValueClause addDatetimeClause(String columnName,
+			Timestamp datetimeValue) {
+		if (keysSb.length() > 0) {
+			keysSb.append(", ");
+			valuesSb.append(", ");
+		}
+		keysSb.append(columnName);
+		if (datetimeValue == null) {
+			valuesSb.append("null");
+		} else {
+			valuesSb.append("?");
+			values.addDatetimeValue(datetimeValue);
+		}
+		return this;
+	}
+
+	public InsertKeyValueClause addClobClause(String columnName,
+			String clobValue) {
+		if (keysSb.length() > 0) {
+			keysSb.append(", ");
+			valuesSb.append(", ");
+		}
+		keysSb.append(columnName);
+		if (clobValue == null) {
+			valuesSb.append("null");
+		} else {
+			valuesSb.append("?");
+			values.addClobValue(clobValue);
+		}
+		return this;
+	}
+
+	public InsertKeyValueClause addBlobClause(String columnName,
+			byte[] blobValue) {
+		if (keysSb.length() > 0) {
+			keysSb.append(", ");
+			valuesSb.append(", ");
+		}
+		keysSb.append(columnName);
+		if (blobValue == null) {
+			valuesSb.append("null");
+		} else {
+			valuesSb.append("?");
+			values.addBlobValue(blobValue);
+		}
+		return this;
+	}
+
+	public String getKeysClause() {
+		return keysSb.toString();
+	}
+
+	public String getValuesClause() {
+		return valuesSb.toString();
+	}
+
+	public Values getValues() {
+		return values;
+	}
+
+}
---
+++ src/org/sosostudio/dbunifier/oom/UpdateKeyValueClause.java
@@ -0,0 +1,97 @@
+package org.sosostudio.dbunifier.oom;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import org.sosostudio.dbunifier.Values;
+
+public class UpdateKeyValueClause {
+
+	private StringBuilder sb = new StringBuilder();
+
+	private Values values = new Values();
+
+	public UpdateKeyValueClause addStringClause(String columnName,
+			String stringValue) {
+		if (sb.length() > 0) {
+			sb.append(", ");
+		}
+		sb.append(columnName).append(" = ");
+		if (stringValue == null) {
+			sb.append("null");
+		} else {
+			sb.append("?");
+			values.addStringValue(stringValue);
+		}
+		return this;
+	}
+
+	public UpdateKeyValueClause addNumberClause(String columnName,
+			BigDecimal numberValue) {
+		if (sb.length() > 0) {
+			sb.append(", ");
+		}
+		sb.append(columnName).append(" = ");
+		if (numberValue == null) {
+			sb.append("null");
+		} else {
+			sb.append("?");
+			values.addNumberValue(numberValue);
+		}
+		return this;
+	}
+
+	public UpdateKeyValueClause addDatetimeClause(String columnName,
+			Timestamp datetimeValue) {
+		if (sb.length() > 0) {
+			sb.append(", ");
+		}
+		sb.append(columnName).append(" = ");
+		if (datetimeValue == null) {
+			sb.append("null");
+		} else {
+			sb.append("?");
+			values.addDatetimeValue(datetimeValue);
+		}
+		return this;
+	}
+
+	public UpdateKeyValueClause addClobClause(String columnName,
+			String clobValue) {
+		if (sb.length() > 0) {
+			sb.append(", ");
+		}
+		sb.append(columnName).append(" = ");
+		if (clobValue == null) {
+			sb.append("null");
+		} else {
+			sb.append("?");
+			values.addClobValue(clobValue);
+		}
+		return this;
+	}
+
+	public UpdateKeyValueClause addBlobClause(String columnName,
+			byte[] blobValue) {
+		if (sb.length() > 0) {
+			sb.append(", ");
+		}
+		sb.append(columnName).append(" = ");
+		if (blobValue == null) {
+			sb.append("null");
+		} else {
+			sb.append("?");
+			values.addBlobValue(blobValue);
+		}
+		return this;
+	}
+
+	public String getClause() {
+		return sb.toString();
+	}
+
+	public Values getValues() {
+		return values;
+	}
+
+}
